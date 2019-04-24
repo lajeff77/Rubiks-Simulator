dca8ca7c609de8b554a9e535e0e07dcab6ceb2fa
lajeff77 <lajeff77@gmail.com>
2019-04-24 16:10:45 -0700

    ver 0.0.0 commit, setting up the project

---
+++ src/Launcher.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Launcher Class</h1>
+ *
+ * <p>This is the starting point of the program.</p>
+ *
+ * @version 0.0.0
+ * @author Lauryn Jefferson
+ */
+public class Launcher {
+
+    /**
+     * <h2>main method</h2>
+     *
+     * <p>Typical main method to run the program.</p>
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Lauryn Jefferson, Rubik's Simulator ver 0.0.0");
+    }
+}
+
